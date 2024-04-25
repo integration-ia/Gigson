24e9a469a4b381a534efd633a736a650a3aba173
integration-ia <156549421+integration-ia@users.noreply.github.com>
2024-04-25 15:47:06 -0300

    " Gigson Web - V.02.001 "

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,15 @@
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { useEffect } from 'react';
 
 const Layout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0,0)
+  }, [pathname])
+
   return (
     <>
         <Navbar />
